b7b28add5e37c381139fab53a189f6445574eee3
joypangilinan <jhoypangilinan.30@gmail.com>
2019-01-07 22:54:02 +0800

    change navigation and continue the my account page

--- web/js/webpagesJS/myaccount.js
+++ web/js/webpagesJS/myaccount.js
@@ -5,11 +5,13 @@ $(document).ready(function () {
         $('.change').hide();
         $('.orderdisplay').hide();
         $('.wishdisplay').hide();
+        $('.payment').hide();
         $('.labell').text('My Profile');
         $('.pro').addClass('active');
         $('.pass').removeClass('active');
         $('.myorder').removeClass('active');
         $('.mywish').removeClass('active');
+        $('.paymentoptions').removeClass('active');
         $('.profile').show();
     });
 
@@ -17,6 +19,7 @@ $(document).ready(function () {
         $('.profile').hide();
         $('.orderdisplay').hide()
         $('.wishdisplay').hide();
+        $('.payment').hide();
         $('.labell').text('Change Password');
         $('.active').removeClass('active');
         $('.pass').addClass('active');
@@ -28,6 +31,7 @@ $(document).ready(function () {
         $('.profile').hide();
         $('.change').hide();
         $('.wishdisplay').hide();
+        $('.payment').hide();
         $('.labell').text('My Orders');
         $('.active').removeClass('active');
         $('.myorder').addClass('active');
@@ -37,10 +41,23 @@ $(document).ready(function () {
   $(document).on('click', '.wish', function () {
         $('.profile').hide();
         $('.change').hide();
-         $('.orderdisplay').hide();
+        $('.orderdisplay').hide();
+        $('.payment').hide();
         $('.labell').text('My Wishlist');
         $('.active').removeClass('active');
         $('.mywish').addClass('active');
         $('.wishdisplay').show();
+    });
+
+
+  $(document).on('click', '.payText', function () {
+        $('.profile').hide();
+        $('.change').hide();
+        $('.wishdisplay').hide();
+         $('.orderdisplay').hide();
+        $('.labell').text('My Payment Options');
+        $('.active').removeClass('active');
+        $('.paymentoptions').addClass('active');
+        $('.payment').show();
     });
 });
