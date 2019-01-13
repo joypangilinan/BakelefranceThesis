9ec6685418dd5e06b0d68ac2a80151b97553b32c
joypangilinan <jhoypangilinan.30@gmail.com>
2019-01-13 22:41:25 +0800

    design for register myaccount and custom not complete

---
+++ CAPSTONE2/js/WebpagesJS/myaccount.js
@@ -0,0 +1,69 @@
+$(document).ready(function () {
+
+    $(document).on('click', '.myprofile', function () {
+           $('.change').hide();
+           $('.orderdisplay').hide();
+           $('.wishdisplay').hide();
+           $('.payment').hide();
+           $('.ordersummary').hide();
+           $('.labell').text('My Profile');
+           $('.pro').addClass('active');
+           $('.pass').removeClass('active');
+           $('.myorder').removeClass('active');
+           $('.mywish').removeClass('active');
+           $('.paymentoptions').removeClass('active');
+           $('.profile').show();
+       });
+   
+    $(document).on('click', '.chPass', function () {
+           $('.profile').hide();
+           $('.orderdisplay').hide()
+           $('.wishdisplay').hide();
+           $('.ordersummary').hide();
+           $('.payment').hide();
+           $('.labell').text('Change Password');
+           $('.active').removeClass('active');
+           $('.pass').addClass('active');
+           $('.change').show();
+       });
+   
+   
+    $(document).on('click', '.ordertext', function () {
+           $('.profile').hide();
+           $('.change').hide();
+           $('.wishdisplay').hide();
+           $('.payment').hide();
+           $('.labell').text('My Orders');
+           $('.active').removeClass('active');
+           $('.myorder').addClass('active');
+           $('.orderdisplay').show();
+           $('.ordersummary').show();
+       });
+   
+     $(document).on('click', '.wish', function () {
+           $('.profile').hide();
+           $('.change').hide();
+           $('.orderdisplay').hide();
+           $('.ordersummary').hide();
+           $('.payment').hide();
+           $('.labell').text('My Wishlist');
+           $('.active').removeClass('active');
+           $('.mywish').addClass('active');
+           $('.wishdisplay').show();
+       });
+   
+   
+     $(document).on('click', '.payText', function () {
+           $('.profile').hide();
+           $('.change').hide();
+           $('.wishdisplay').hide();
+           $('.ordersummary').hide();
+           $('.orderdisplay').hide();
+           $('.labell').text('My Payment Options');
+           $('.active').removeClass('active');
+           $('.paymentoptions').addClass('active');
+           $('.payment').show();
+       });
+   });
+
+   
